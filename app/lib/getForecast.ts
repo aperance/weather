baeae086d1fe819c5f3ab49048cdffe62b9e0f02
William Aperance <aperance@gmail.com>
2024-02-07 16:42:16 -0700

    Improved data parsing

--- app/lib/getForecast.ts
+++ app/lib/getForecast.ts
@@ -2,30 +2,56 @@ import { XMLParser } from "fast-xml-parser";
 import { z } from "zod";
 
 const schema = z.object({
-  data: z.object({
-    "time-layout": z.object({
-      "start-valid-time": z.array(z.coerce.date()),
-    }),
-    parameters: z.object({
-      weather: z.object({
-        "weather-conditions": z.array(
-          z.object({
-            value: z
-              .array(
-                z.object({
-                  type: z.string(),
-                  coverage: z.string(),
-                })
-              )
-              .default([]),
-          })
-        ),
+  data: z
+    .object({
+      "time-layout": z.object({
+        "start-valid-time": z.array(z.coerce.date()),
       }),
-    }),
-  }),
+      parameters: z.object({
+        temperature: z
+          .array(z.object({ value: z.array(z.number()), type: z.string() }))
+          .transform((arr) =>
+            Object.fromEntries(arr.map(({ type, value }) => [type, value]))
+          )
+          .pipe(
+            z.object({
+              hourly: z.array(z.number()),
+              "wind chill": z.array(z.number()),
+              "dew point": z.array(z.number()),
+            })
+          ),
+        weather: z.object({
+          "weather-conditions": z.array(
+            z.object({
+              value: z
+                .array(
+                  z.object({
+                    type: z.string(),
+                    coverage: z.string(),
+                  })
+                )
+                .default([]),
+            })
+          ),
+        }),
+      }),
+    })
+    .transform((obj) => ({
+      labels: obj["time-layout"]["start-valid-time"],
+      temperature: obj.parameters.temperature.hourly,
+      windChill: obj.parameters.temperature["wind chill"],
+      dewPoint: obj.parameters.temperature["dew point"],
+      thunderstorms: obj.parameters.weather["weather-conditions"].map(
+        ({ value }) =>
+          value.find(({ type }) => type === "thunderstorms")?.coverage ?? ""
+      ),
+      snow: obj.parameters.weather["weather-conditions"].map(
+        ({ value }) => value.find(({ type }) => type === "snow")?.coverage ?? ""
+      ),
+    })),
 });
 
-const parser = new XMLParser({
+const xmlParser = new XMLParser({
   ignoreDeclaration: true,
   ignoreAttributes: false,
   attributeNamePrefix: "",
@@ -41,20 +67,8 @@ export default async function getForecast() {
     { cache: "no-cache" }
   );
   const xmlText = await xmlFetch.text();
-  const obj = parser.parse(xmlText, true).dwml;
+  const obj = xmlParser.parse(xmlText, true).dwml;
+  console.log(obj.data);
   const validatedObj = schema.parse(obj);
-
-  const timestamps = validatedObj.data["time-layout"]["start-valid-time"];
-  const weatherConditions =
-    validatedObj.data.parameters.weather["weather-conditions"];
-
-  const y = timestamps.map((timestamp, i) => {
-    return {
-      timestamp,
-      values: weatherConditions[i].value,
-    };
-  });
-
-  console.log(obj);
-  console.log(y);
+  console.log({ validatedObj });
 }
