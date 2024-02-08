fd057713226452c6510113dea48580d5155b07b1
William Aperance <aperance@gmail.com>
2024-02-07 21:29:35 -0700

    Improved zod schema

--- app/lib/getForecast.ts
+++ app/lib/getForecast.ts
@@ -1,25 +1,47 @@
 import { XMLParser } from "fast-xml-parser";
 import { z } from "zod";
 
+const valuesSchema = (type: z.ZodTypeAny) =>
+  z.array(z.object({ node: type }).transform((o) => o.node));
+
+const valueArrayToObject = (arr: any[]) =>
+  Object.fromEntries(arr.map(({ type, value }) => [type, value]));
+
 const schema = z.object({
   data: z
     .object({
       "time-layout": z.object({
-        "start-valid-time": z.array(z.coerce.date()),
+        "start-valid-time": valuesSchema(z.coerce.date()),
       }),
       parameters: z.object({
         temperature: z
-          .array(z.object({ value: z.array(z.number()), type: z.string() }))
-          .transform((arr) =>
-            Object.fromEntries(arr.map(({ type, value }) => [type, value]))
-          )
-          .pipe(
+          .array(
             z.object({
-              hourly: z.array(z.number()),
-              "wind chill": z.array(z.number()),
-              "dew point": z.array(z.number()),
+              value: valuesSchema(z.number().optional()),
+              type: z.string(),
             })
-          ),
+          )
+          .transform(valueArrayToObject),
+        "wind-speed": z
+          .array(
+            z.object({
+              value: valuesSchema(z.number().optional()),
+              type: z.string(),
+            })
+          )
+          .transform(valueArrayToObject),
+        "probability-of-precipitation": z.object({
+          value: valuesSchema(z.number().optional()),
+        }),
+        "cloud-amount": z.object({
+          value: valuesSchema(z.number().optional()),
+        }),
+        humidity: z.object({
+          value: valuesSchema(z.number().optional()),
+        }),
+        direction: z.object({
+          value: valuesSchema(z.number().optional()),
+        }),
         weather: z.object({
           "weather-conditions": z.array(
             z.object({
@@ -41,6 +63,15 @@ const schema = z.object({
       temperature: obj.parameters.temperature.hourly,
       windChill: obj.parameters.temperature["wind chill"],
       dewPoint: obj.parameters.temperature["dew point"],
+      humidity: obj.parameters.humidity.value,
+      precipitation: obj.parameters["probability-of-precipitation"].value,
+      cloudCover: obj.parameters["cloud-amount"].value,
+      windDirection: obj.parameters.direction.value,
+      windSustained: obj.parameters["wind-speed"].sustained,
+      windGusts: obj.parameters["wind-speed"].gust,
+      rain: obj.parameters.weather["weather-conditions"].map(
+        ({ value }) => value.find(({ type }) => type === "rain")?.coverage ?? ""
+      ),
       thunderstorms: obj.parameters.weather["weather-conditions"].map(
         ({ value }) =>
           value.find(({ type }) => type === "thunderstorms")?.coverage ?? ""
@@ -48,17 +79,34 @@ const schema = z.object({
       snow: obj.parameters.weather["weather-conditions"].map(
         ({ value }) => value.find(({ type }) => type === "snow")?.coverage ?? ""
       ),
-    })),
+    }))
+    .pipe(
+      z.object({
+        labels: z.array(z.date()),
+        temperature: z.array(z.number().optional()),
+        windChill: z.array(z.number().optional()),
+        dewPoint: z.array(z.number().optional()),
+        humidity: z.array(z.number().optional()),
+        precipitation: z.array(z.number().optional()),
+        cloudCover: z.array(z.number().optional()),
+        windDirection: z.array(z.number().optional()),
+        windSustained: z.array(z.number().optional()),
+        windGusts: z.array(z.number().optional()),
+        rain: z.array(z.string()),
+        thunderstorms: z.array(z.string()),
+        snow: z.array(z.string()),
+      })
+    ),
 });
 
 const xmlParser = new XMLParser({
   ignoreDeclaration: true,
   ignoreAttributes: false,
   attributeNamePrefix: "",
+  alwaysCreateTextNode: true,
+  textNodeName: "node",
   isArray: (name) => name === "value",
-  transformAttributeName: (name) => {
-    return name === "weather-type" ? "type" : name;
-  },
+  transformAttributeName: (name) => (name === "weather-type" ? "type" : name),
 });
 
 export default async function getForecast() {
@@ -70,5 +118,5 @@ export default async function getForecast() {
   const obj = xmlParser.parse(xmlText, true).dwml;
   console.log(obj.data);
   const validatedObj = schema.parse(obj);
-  console.log({ validatedObj });
+  console.log(validatedObj);
 }
